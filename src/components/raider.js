a9d82e2ce84a0de180f0b6cbcd7ae322e3a6492b
Simo Ojala/Nekuin <Simo.ojala@metropolia.fi>
2020-10-18 18:37:05 +0300

    truncate long names

--- src/components/raider.js
+++ src/components/raider.js
@@ -9,7 +9,14 @@ const Raider = ({ raider }) => {
                 className="row raider mx-auto mb-1"
                 style={{ backgroundColor: classColors[raider.class] }}
             >
-                <p>{raider.name}</p>
+                <p>
+                    {raider.name.length > 13
+                        ? // truncate really long names
+                          // I chose 13, cause thats the longest spec name
+                          // (assassination)
+                          raider.name.substring(0, 13)
+                        : raider.name}
+                </p>
                 <p>{raider.class}</p>
                 <p>{raider.spec}</p>
             </div>
